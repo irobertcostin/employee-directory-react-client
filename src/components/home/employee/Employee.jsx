8b90463a66d66559c38dd4878c9b66061636b22a
irobertcostin <irobertcostin@gmail.com>
2023-03-06 21:52:08 +0200

    first commit

---
+++ src/components/home/employee/Employee.jsx
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+
+function Employee({employee}){
+
+    return (
+
+        <div className="employee-card" key={employee.id}>
+
+
+
+
+        </div>
+
+
+
+    )
+
+
+
+}
+
+
+export default Employee;
